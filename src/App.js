7e589a3ea749c8015d352e166a15f7396b6a1fcb
Parisa Sohrabi <parisahs@gmail.com>
2022-09-07 18:32:38 -0400

    added a form and used useState to bind the inputs with states

--- src/App.js
+++ src/App.js
@@ -1,11 +1,69 @@
 
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cPassword, setCPassword] = useState("");
+
+  const formHandler = (e) => {
+    e.preventDefault();
+    const newUsers = [...users];
+    newUsers.push({ fname, lname, email, password, cPassword });
+    setUsers(newUsers);
+    setFname("");
+    setLname("");
+    setEmail("");
+    setPassword("");
+    setCPassword("");
+  };
   return (
-    <div className="App">
-     
-    </div>
+    <form action="" className="form" onSubmit={formHandler}>
+      <div className="input-group">
+        <label htmlFor="">First Name</label>
+        <input
+          type="text"
+          value={fname}
+          onChange={(e) => setFname(e.target.value)}
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="">Last Name</label>
+        <input
+          type="text"
+          value={lname}
+          onChange={(e) => setLname(e.target.value)}
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="">Email</label>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="">Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="">Confirm Password</label>
+        <input
+          type="password"
+          value={cPassword}
+          onChange={(e) => setCPassword(e.target.value)}
+        />
+      </div>
+    </form>
   );
 }
 
